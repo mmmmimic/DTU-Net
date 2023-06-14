d0b1e5f6b212945e2391817f5cb80bc01ef10e18
Manxi Lin <46046604+mmmmimic@users.noreply.github.com>
2023-06-14 10:44:05 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from .unet_family import RegUNet, ResUNet, FlexUNet, DTUNet
 from .smp_models import SMPUNet, SMPDeepLab, SMPFCN, SMPPSPNet
-from .SASceneNet import SASNet
 from .activations import fetal_concept_act, fetal_concept_act_womask, fetal_caliper_concept
 from .backbones import ResNet, Inception
